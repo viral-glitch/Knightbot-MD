d02c47024cc954b19194c3eaab9e3c57254b8550
Flavie Kshlerin <viraldeaura@gmail.com>
2025-07-13 21:15:46 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Viral Bot',
   author: '‎',
-  botName: "James",
+  botName: "Zenji",
   botOwner: 'Vai', // Your name
-  ownerNumber: '255658032461', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '255620847449', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
