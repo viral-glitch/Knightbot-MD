52f2832fc8e327840910f1e4d1266a613daf9156
Flavie Kshlerin <viraldeaura@gmail.com>
2025-07-21 13:21:23 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Viral Bot',
+  packname: 'Dox Bot',
   author: '‎',
-  botName: "Zenji",
+  botName: "Dox",
   botOwner: 'Vai', // Your name
-  ownerNumber: '255620847449', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '255687983060', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
